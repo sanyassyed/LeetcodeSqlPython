f0824db44d505d44f8ca58c577d5c83dda653d4d
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-05 16:35:40 -0500

    Time: 255 ms (37.74%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,15 +1,7 @@
 # Write your MySQL query statement below
-WITH cte 
-AS
-(
-SELECT machine_id,
-       process_id,
-       SUM(IF(activity_type LIKE 'start', timestamp * -1, timestamp)) processing_time
-FROM Activity
-GROUP BY machine_id, 
-         process_id
-)
-SELECT machine_id,
-       ROUND(AVG(processing_time),3) processing_time
-FROM cte
-GROUP BY machine_id;
+SELECT a1.machine_id,
+       ROUND(AVG(a2.timestamp - a1.timestamp), 3) processing_time
+FROM Activity a1 INNER JOIN 
+     Activity a2 ON
+     a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id AND a1.activity_type LIKE 'start' AND a2.activity_type LIKE 'end'
+GROUP BY a1.machine_id;
