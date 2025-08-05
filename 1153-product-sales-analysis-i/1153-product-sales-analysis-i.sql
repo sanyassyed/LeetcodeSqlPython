7bbfa867019764629f343fb1fe3918375b02f98c
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-05 15:56:04 -0500

    Time: 1188 ms (71.71%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT p.product_name,
-       s.year,
-       s.price
-FROM Sales s LEFT JOIN
+SELECT product_name,
+       year,
+       price
+FROM Sales s,
      Product p
-     ON s.product_id = p.product_id;
+WHERE s.product_id = p.product_id;
