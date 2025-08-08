fa4373f112447db4b965e8766d0af4b540b95ac9
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-08 10:26:42 -0500

    Time: 11 ms (72.17%), Space: 17.8 MB (64.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        h_s = {}
-        h_t = {}
-        l_s = len(s)
-        l_t = len(t)
+        vals = [0] * 26
+        ord_a = ord('a')
 
-        if l_s != l_t:
+        if len(s) != len(t):
             return False
-        
-        for i in range(l_s):
-            c_s = s[i]
-            h_s[c_s] = h_s.get(c_s, 0) + 1
-            c_t = t[i] 
-            h_t[c_t] = h_t.get(c_t, 0) + 1
 
+        for c in s:
+            ord_c = ord(c) - ord_a
+            vals[ord_c] += 1
 
-        for k in h_s:
-            if h_s[k] != h_t.get(k, 0):
+        for c in t:
+            ord_c = ord(c) - ord_a
+            if vals[ord_c] != 0:
+                vals[ord_c] -= 1
+            else:
                 return False
 
-        return True       
+        return True
