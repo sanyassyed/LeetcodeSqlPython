cec08b44d5aeeee46af4cbebba481ad1773733d1
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-13 11:48:25 -0500

    Time: 647 ms (59.2%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
 SELECT s.user_id,
-       ROUND(SUM(IF(c.action LIKE 'timeout' OR c.action IS NULL, 0, 1)) / COUNT(*), 2) confirmation_rate
+       ROUND(SUM(IF(c.action = 'confirmed', 1, 0)) / COUNT(*), 2) confirmation_rate
 FROM Signups s LEFT JOIN
-     Confirmations c ON
-     s.user_id = c.user_id
+     Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
+
+-- SELECT *
+-- FROM Signups s LEFT JOIN
+--      Confirmations c ON s.user_id = c.user_id;
+
+
