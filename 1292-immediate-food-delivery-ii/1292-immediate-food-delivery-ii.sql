8f69c068ea72cfea6d78b939a6a7d648f6e607ba
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-18 09:12:12 -0500

    Time: 658 ms (41.5%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,11 +1,26 @@
 # Write your MySQL query statement below
+-- WITH cte
+-- AS
+-- (
+-- SELECT *,
+--        FIRST_VALUE(order_date) OVER (PARTITION BY customer_id ORDER BY order_date ASC) first_order
+-- FROM Delivery
+-- )
+-- SELECT ROUND((SUM(IF(order_date = customer_pref_delivery_date, 1, 0)) / COUNT(*)) * 100, 2) immediate_percentage
+-- FROM cte
+-- WHERE order_date = first_order;
+
+-- ROUND((SUM(IF(d.order_date = d.customer_pref_delivery_date, 1, 0)) / COUNT(*) ) * 100 , 2) immediate_percentage,
+
 WITH cte
 AS
 (
-SELECT *,
-       FIRST_VALUE(order_date) OVER (PARTITION BY customer_id ORDER BY order_date ASC) first_order
+SELECT customer_id,
+       MIN(order_date) first_order
 FROM Delivery
+GROUP BY customer_id
 )
-SELECT ROUND((SUM(IF(order_date = customer_pref_delivery_date, 1, 0)) / COUNT(*)) * 100, 2) immediate_percentage
-FROM cte
-WHERE order_date = first_order;
+SELECT ROUND((SUM(IF(d.order_date = d.customer_pref_delivery_date, 1, 0)) / COUNT(*)) * 100 , 2) immediate_percentage
+FROM Delivery d,
+     cte c
+WHERE d.order_date = c.first_order AND c.customer_id = d.customer_id;
