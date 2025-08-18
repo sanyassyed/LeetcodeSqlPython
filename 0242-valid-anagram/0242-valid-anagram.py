161dd67789d92a0b491f533daa8253265c421a2d
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-18 11:02:20 -0500

    Time: 18 ms (18.56%), Space: 17.8 MB (64.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        '''
         l_s = len(s)
         l_t = len(t)
 
@@ -19,6 +20,27 @@ class Solution:
             maps[ord(c) - ord_a] -= 1
 
         return True
+        '''
+        l_s = len(s)
+        l_t = len(t)
+        if l_s != l_t:
+            return False
+        map_s, map_t = {}, {}
+
+        for i in range(l_s):
+            c_s = s[i]
+            c_t = t[i]
+
+            map_s[c_s] = map_s.get(c_s, 0) + 1
+            map_t[c_t] = map_t.get(c_t, 0) + 1
+
+        for k,v in map_s.items():
+            if map_t.get(k, 0) != v:
+                return False
+
+        return True
+
+            
 
         
             
