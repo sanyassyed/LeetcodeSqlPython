a8c47a911a92a465c0130aa0c50b207abbeac518
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-08-20 08:53:59 -0500

    Time: 603 ms (34.05%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -2,4 +2,4 @@
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product);
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
