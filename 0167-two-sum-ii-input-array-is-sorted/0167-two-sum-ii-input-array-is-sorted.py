5ddda8612ce3c51d68da485826b9a80498fd207e
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-09-06 18:28:22 -0500

    Time: 3 ms (80.71%), Space: 18.5 MB (90.09%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,9 +4,10 @@ class Solution:
         for i in range(len(numbers)):
             diff = target - numbers[i]
             if diff in hash_nums:
-                ind = hash_nums[diff] + 1
-                if ind >=1 and ind <= i+1:
-                    return [ind, i+1]
+                return [hash_nums[diff]+1, i+1]
+                #ind = hash_nums[diff] + 1
+                #if ind >=1 and ind <= i+1:
+                    #return [ind, i+1]
                 
             hash_nums[numbers[i]] = i
 
