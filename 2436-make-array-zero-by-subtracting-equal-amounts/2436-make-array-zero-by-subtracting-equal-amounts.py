f0d68812e4cb33a93fb1d8ab6072ac47fb5c8be2
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2025-10-09 11:25:19 -0500

    Time: 11 ms (6.13%), Space: 18.1 MB (10.72%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,3 +1,13 @@
+def find_min(nums):
+    min_n = 0
+    for n in nums:
+        if min_n == 0 and n != 0:
+            min_n = n
+        elif n != 0:
+            min_n = min(min_n, n)
+    return min_n
+        
+
 class Solution:
     def minimumOperations(self, nums):
         """
@@ -6,10 +16,10 @@ class Solution:
         """
         counter = 0
         while True:
-            non_zero_nums = [x for x in nums if x != 0]
-            if non_zero_nums == []:
+            min_n = find_min(nums)
+            print(min_n)
+            if min_n == 0:
                 return counter
-            min_n = min(non_zero_nums)
             nums = [x-min_n if x !=0 else x for x in nums]
             counter += 1
 
