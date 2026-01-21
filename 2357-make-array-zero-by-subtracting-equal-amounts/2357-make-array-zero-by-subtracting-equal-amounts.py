ed61a46fc61ea18ff4236863961d5924052cb1d6
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-01-21 12:41:35 -0600

    Time: 8 ms (7.81%), Space: 19.4 MB (15.33%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,19 +1,13 @@
-def find_min(nums):
-    least = 0
-    if len(nums) != 0:
-        least = nums[0]
-        for n in range(1, len(nums)):
-            least = min(nums[n], least)
-    return least
 class Solution:
-    def minimumOperations(self, nums: List[int]) -> int: 
+    def minimumOperations(self, nums: List[int]) -> int:
         counter = 0
         while True:
-            small = find_min([x for x in nums if x != 0])
-            if small == 0:
+            no_zero = [x for x in nums if x != 0]
+            if no_zero:
+                least = min(no_zero)
+                nums = [x-least if x != 0 else x for x in nums]
+                counter += 1
+            else:
                 return counter
-            nums = [x-small if x != 0 else x for x in nums] 
-            counter += 1
-
-        return counter
         
+        return counter
