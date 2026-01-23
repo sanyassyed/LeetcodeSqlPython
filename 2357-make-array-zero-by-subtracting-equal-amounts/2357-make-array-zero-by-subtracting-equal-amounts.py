e077af40051ca3a6099bf3e2a386740089e75faa
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-01-23 14:27:05 -0600

    Time: 3 ms (24.23%), Space: 19.3 MB (19.01%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -3,11 +3,9 @@ class Solution:
         counter = 0
         while True:
             no_zero = [x for x in nums if x != 0]
-            if no_zero:
-                least = min(no_zero)
-                nums = [x-least if x != 0 else x for x in nums]
+            if len(no_zero) != 0:
+                small = min(no_zero)
+                nums = [x-small if x !=0 else x for x in nums]
                 counter += 1
             else:
                 return counter
-        
-        return counter
