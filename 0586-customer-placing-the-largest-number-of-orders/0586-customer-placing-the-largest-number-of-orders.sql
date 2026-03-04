42bb1afe7aa4182bd2749b96fa80bfaa677b7489
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-03-04 15:47:13 -0600

    Time: 435 ms (74.4%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+-- SELECT customer_number
+-- FROM Orders
+-- GROUP BY customer_number
+-- ORDER BY COUNT(order_number) DESC
+-- LIMIT 1;
+WITH order_count_cte
+AS
+(
+SELECT customer_number,
+       COUNT(order_number) total_orders
+FROM Orders
+GROUP BY customer_number
+),
+customer_ranked
+AS
+(
+SELECT customer_number,
+       DENSE_RANK() OVER (ORDER BY total_orders DESC) rnk
+FROM order_count_cte
+)
+SELECT customer_number
+FROM customer_ranked
+WHERE rnk = 1;
