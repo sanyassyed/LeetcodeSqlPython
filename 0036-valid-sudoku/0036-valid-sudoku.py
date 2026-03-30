ec80347604c506407916328c175c0f6887a3803d
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-03-30 14:42:38 -0500

    Time: 7 ms (30.26%), Space: 19.4 MB (25.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,21 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        seen_r = defaultdict(list)
-        seen_c = defaultdict(list)
-        seen_sq = defaultdict(list)
+        seen_r = defaultdict(set)
+        seen_c = defaultdict(set)
+        seen_sq = defaultdict(set)
         for r in range(0,9):
             for c in range(0,9):
                 item = board[r][c]
                 if item == '.':
                     continue
-                if item in seen_r[r]:
+                if (item in seen_r[r] or
+                   item in seen_c[c] or
+                   item in seen_sq[(r//3,c//3)]):
                     return False
-                if item in seen_c[c]:
-                    return False
-                if item in seen_sq[(r//3,c//3)]:
-                    return False
-                seen_r[r].append(item)
-                seen_c[c].append(item)
-                seen_sq[(r//3, c//3)].append(item)
+                seen_r[r].add(item)
+                seen_c[c].add(item)
+                seen_sq[(r//3, c//3)].add(item)
             
         return True
