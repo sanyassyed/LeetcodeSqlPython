638ff5f5f2f4d5e2a3b9fe365e9e51033737ced5
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-03-31 09:11:26 -0500

    Time: 259 ms (43.21%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Cinema
-WHERE id % 2 != 0 AND
-      description NOT LIKE '%boring%'
+WHERE id%2 = 1 AND
+      NOT REGEXP_LIKE(description, "^boring$")
 ORDER BY rating DESC;
