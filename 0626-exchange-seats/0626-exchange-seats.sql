a8358807013e24a495fc35dc93fc7f93c7d773a9
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-03-31 09:18:12 -0500

    Time: 383 ms (15.22%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
-WITH detail_seat_cte
+WITH detail_seat
 AS
 (
 SELECT *,
-       LEAD(student) OVER w next,
-       LAG(student) OVER w prev
+       LAG(id) OVER w prev,
+       LEAD(id) OVER w next
 FROM Seat
 WINDOW w AS (ORDER BY id ASC)
 )
-SELECT id,
-       IFNULL(IF(id%2 = 0, prev, next), student) student
-FROM detail_seat_cte
-ORDER BY id ASC;
+SELECT IFNULL(IF(id%2 = 0, prev, next), id) id,
+       student
+FROM detail_seat
+ORDER BY 1 ASC;
