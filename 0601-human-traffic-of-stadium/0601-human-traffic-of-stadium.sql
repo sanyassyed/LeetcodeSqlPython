c51b4753b51fae902a803f06be9242a8efe6ccb4
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2026-04-14 11:16:16 -0500

    Time: 407 ms (16.59%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,24 +1,21 @@
 # Write your MySQL query statement below
-WITH detailed_stadium
-AS
+WITH cte
+As
 (
 SELECT *,
+       LAG(id) OVER w before_1,
+       LAG(id, 2) OVER w before_2,
        LEAD(id) OVER w next_1,
-       LEAD(id, 2) OVER w next_2,
-       LAG(id) OVER w prev_1,
-       LAG(id, 2) OVER w prev_2
+       LEAD(id, 2) OVER w next_2
 FROM Stadium
 WHERE people >= 100
 WINDOW w AS (ORDER BY id ASC)
+ORDER BY id ASC
 )
 SELECT id,
-       visit_date,
-       people
-FROM detailed_stadium 
-WHERE (next_1 = id + 1 AND
-      next_2 = id + 2) OR
-      (prev_1 = id-1 AND
-      prev_2 = id - 2) OR
-      (next_1 = id +1 AND
-      prev_1 = id -1)
-ORDER BY visit_date ASC;
+                visit_date,
+                people
+FROM cte
+WHERE (before_1 = id -1 AND before_2 = id - 2) OR
+      (before_1 = id-1 AND next_1 = id +1) OR 
+      (next_2 = id+2 AND next_1 = id +1);
